01046a26d57e7a7d7863212167640131c5f383cd
chandramani12 <chandramaninet1002@gmail.com>
2023-05-14 00:12:42 +0530

    Clod Based Java Project firs commit

---
+++ Cloud Based Java Project and Practicle/tcpServerPrime.java
@@ -0,0 +1,38 @@
+import java.net.*;
+import java.io.*;
+class tcpServerPrime
+{
+	public static void main (String args[])
+	{
+		try
+		{
+			ServerSocket ss= new ServerSocket (8001);
+			System.out.println("Chandramani server started...");
+			Socket s = ss.accept();
+			DataInputStream in = new DataInputStream (s.getInputStream());
+			int x = in.readInt();
+			DataOutputStream out = new DataOutputStream(s.getOutputStream());
+			int y = x/2;
+			if(x==1 ||x==2 ||x==3)
+			{
+				out.writeUTF(x+ " is prime number");
+				System.exit(0);
+			}
+			for(int i = 2; i<=y; i++)
+			{
+				if(x%i != 0)
+				{
+					out.writeUTF(x+ " is prime number");
+				}
+				else
+				{
+					out.writeUTF(x+ " is not prime number");
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.toString());
+		}
+	}
+}
